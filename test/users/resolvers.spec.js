e8f3dfad41cd11b085fe8d86f94f410559a1ebfe
Emanuel Casco <cascoemanuel@gmail.com>
2019-07-29 16:40:32 -0400

    Kickoff

---
+++ test/users/resolvers.spec.js
@@ -0,0 +1,36 @@
+const userFactory = require('../factories/user'),
+  { mutations } = require('../../app/graphql/users/mutations');
+
+describe('users', () => {
+  describe('resolvers', () => {
+    describe('createUser', () => {
+      it('should create an user successfuly', async done => {
+        const user = await userFactory.build();
+        return mutations.createUser({}, { user: user.dataValues }).then(res => {
+          expect(res.dataValues).toHaveProperty('id');
+          expect(res.dataValues).toHaveProperty('firstName');
+          expect(res.dataValues).toHaveProperty('lastName');
+          expect(res.dataValues).toHaveProperty('email');
+          expect(res.dataValues).toHaveProperty('username');
+          expect(res.dataValues).toHaveProperty('password');
+          expect(res.dataValues).toHaveProperty('updatedAt');
+          expect(res.dataValues).toHaveProperty('createdAt');
+          done();
+        });
+      });
+
+      it('should fail to create an user with malformed parameters', done => {
+        mutations
+          .createUser({}, { user: { a: 'b' } })
+          .then(res => {
+            done(res);
+          })
+          .catch(err => {
+            expect(typeof err.errors).toBe('object');
+            expect(err.errors).toHaveLength(5);
+            done();
+          });
+      });
+    });
+  });
+});
